84a9299df489fa3b1f6e74b87b128efd8cd2b0e5
Francesco Generali <genemania@hotmail.it>
2021-11-13 13:16:28 +0100

    WallTunneling script + Import TextMeshPro

---
+++ Assets/Scripts/WallTunneling.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class WallTunneling : MonoBehaviour
+{
+    public TextMeshProUGUI textCanvas;
+    private GameObject wall;
+    private bool isCrossing;
+    private Rigidbody rbPlayer;
+
+    private void Awake()
+    {
+        rbPlayer = GetComponent<Rigidbody>();
+        wall = GameObject.FindGameObjectWithTag("Glass");
+    }
+
+
+    void Update()
+    {
+        if (textCanvas.gameObject.activeInHierarchy == true && !isCrossing)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                GetComponent<PlayerController>().enabled = false;
+                StartCoroutine(animationCrossWall());
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Glass")
+        {
+            textCanvas.gameObject.SetActive(true);
+            textCanvas.text = "Press E to try to cross the wall";
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Glass")
+        {
+            textCanvas.gameObject.SetActive(false);
+        }
+    }
+
+    IEnumerator animationCrossWall()
+    {
+        int attempts = Random.Range(2, 5);
+        for (int i = 0; i < attempts; i++)
+        {
+            rbPlayer.AddForce(transform.forward * 600);
+            yield return new WaitForSeconds(0.3f);
+            rbPlayer.AddForce(-transform.forward * 300);
+            yield return new WaitForSeconds(0.5f);
+        }
+        wall.GetComponent<MeshCollider>().enabled = false;
+        rbPlayer.AddForce(transform.forward * 700);
+        yield return new WaitForSeconds(0.5f);
+
+        wall.GetComponent<MeshCollider>().enabled = true;
+        GetComponent<PlayerController>().enabled = true;
+    }
+}
