46ed2b4b8bf0560e9c2d5b9304208d1f058ac3af
Luca Ballarati <1uca8allarati@gmail.com>
2021-11-13 15:35:38 +0100

    added player splitting, work in progress
    ho aggiunto a PlayerController delle isturzioni che, se il giocater clicca E mentre guarda un oggetto vicino che ha lo script PlayerSplitter, crea una sua copia alla posizione salvata in PlayerSplitter

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,17 +17,39 @@ public class PlayerController : MonoBehaviour
 
     public GameObject cam;
     public GameObject quantumManager;
+    public GameObject playerPrefab;
 
     private bool grabbing;
     private bool onGround;
-    private bool tryingToTunnel;
-    private bool b;
+    private bool isItSplit;
+    private bool isTheCopy;
 
     private GameObject grabbedObject;
 
     private Rigidbody rb;
     private QuantumManager qm;
-    public void SetTryingToTunnel(bool value) { tryingToTunnel = value; }
+
+    public bool GetIsTheCopy() { return isTheCopy; }
+
+    public void SetIsTheCopy(bool value) { isTheCopy = value; }
+
+    public void Split() 
+    {
+        Debug.Log("split!");
+        GetComponent<MeshRenderer>().enabled = true;
+        if (!isItSplit)
+        {
+            isItSplit = true;
+            if (!isTheCopy) cam.GetComponent<Camera>().rect.Set(0.0f, 0.0f, 0.5f, 1.0f);
+            else cam.GetComponent<Camera>().rect.Set(0.5f, 0.0f, 0.5f, 1.0f);
+        }
+        else
+        {
+            if (isTheCopy) Destroy(gameObject);
+            isItSplit = false;
+            cam.GetComponent<Camera>().rect.Set(0.0f, 0.0f, 1.0f, 1.0f);
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -37,43 +59,46 @@ public class PlayerController : MonoBehaviour
 
         grabbing = false;
         onGround = true;
-        tryingToTunnel = false;
-        b = false;
+        isItSplit = false;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (!tryingToTunnel)
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            if (Input.GetKeyDown(KeyCode.E))
+            if (grabbing)
             {
-                if (grabbing)
+                grabbedObject.GetComponent<Rigidbody>().isKinematic = false;
+                grabbedObject.transform.SetParent(null);
+                grabbing = false;
+            }
+            else
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, grabRange))
                 {
-                    grabbedObject.GetComponent<Rigidbody>().isKinematic = false;
-                    grabbedObject.transform.SetParent(null);
-                    grabbing = false;
-                }
-                else
-                {
-                    RaycastHit hit;
-                    if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, grabRange))
+                    if (hit.collider.gameObject.GetComponent<QuantumManager>())
                     {
-                        if (/*(hit.collider.gameObject.GetComponent<WallTunnelling>()*/b && qm.GetQuantum())
-                        {
-                            tryingToTunnel = true;
-                        }
-                        else if (hit.collider.gameObject.GetComponent<QuantumManager>())
-                        {
-                            hit.collider.gameObject.GetComponent<QuantumManager>().ToggleQuantum();
-                        }
-                        else if (hit.collider.gameObject.GetComponent<Rigidbody>())
-                        {
-                            grabbedObject = hit.collider.gameObject;
-                            grabbedObject.GetComponent<Rigidbody>().isKinematic = true;
-                            grabbedObject.transform.SetParent(cam.transform);
-                            grabbing = true;
-                        }
+                        hit.collider.gameObject.GetComponent<QuantumManager>().ToggleQuantum();
+                    }
+                    else if (hit.collider.gameObject.GetComponent<Rigidbody>())
+                    {
+                        grabbedObject = hit.collider.gameObject;
+                        grabbedObject.GetComponent<Rigidbody>().isKinematic = true;
+                        grabbedObject.transform.SetParent(cam.transform);
+                        grabbing = true;
+                    }
+                    else if (hit.collider.gameObject.GetComponent<PlayerSplitter>()) 
+                    {
+                        GameObject copy = Instantiate(playerPrefab, hit.collider.gameObject.GetComponent<PlayerSplitter>().copySpawnPosition, transform.rotation);
+                        copy.transform.GetChild(0).transform.rotation = cam.transform.rotation;
+                        copy.transform.GetChild(0).GetComponent<AudioListener>().enabled = false;
+                        copy.GetComponent<WallTunneling>().textCanvas = GetComponent<WallTunneling>().textCanvas;
+                        copy.GetComponent<PlayerController>().quantumManager = quantumManager;
+                        copy.GetComponent<PlayerController>().SetIsTheCopy(true);
+                        copy.GetComponent<PlayerController>().Split();
+                        Split();
                     }
                 }
             }
---
+++ Assets/Scripts/PlayerSplitter.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSplitter : MonoBehaviour
+{
+    public Vector3 copySpawnPosition;
+}
